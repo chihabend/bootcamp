8cfc14e76894e6d201c9e346d802e3b53a35997a
chihab eddine yassir <chihab.eddine.yassir123@gmail.com>
2025-06-17 22:38:47 +0100

    fix cmnnt

--- week1/day1/challenge/defi.py
+++ week1/day1/challenge/defi.py
@@ -1,10 +1,10 @@
 # defi 1
-# number = int(input("Entrez un nombre : "))
-# length = int(input("Entrez la longueur de la liste : "))
-# multiples = []
-# for i in range(1, length + 1):
-#     multiples.append(number * i)
-# print(multiples)
+number = int(input("Entrez un nombre : "))
+length = int(input("Entrez la longueur de la liste : "))
+multiples = []
+for i in range(1, length + 1):
+    multiples.append(number * i)
+print(multiples)
 # defi 2
 chaine = input("Entrez une chaine charachtere : ")
 def supprimer_repetitions(chaine):
